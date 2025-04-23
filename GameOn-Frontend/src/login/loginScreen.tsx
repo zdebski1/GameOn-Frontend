0c2a5ef0cb4b60672b00dd20f0972f727449414d
zdebs <zdebski1@gmail.com>
2025-04-23 12:55:47 -0400

    updating /authenticate

--- GameOn-Frontend/src/login/loginScreen.tsx
+++ GameOn-Frontend/src/login/loginScreen.tsx
@@ -14,7 +14,7 @@ export default function LoginScreen({ onLogin }: { onLogin: () => void }) {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('http://localhost:3000/authenticate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
